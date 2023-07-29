50b0095d33f870d5611316b2e8b11fd003fe9fbd
C12 <mcpartlandpatrick@gmail.com>
2023-07-29 14:24:32 -0400

    removed post model, added comment and post model

--- models/index.js
+++ models/index.js
@@ -1,13 +1,39 @@
 const User = require('./User');
-const Project = require('./Project');
+const Post = require('./Post');
+const Comment = require('./Comment');
 
-User.hasMany(Project, {
+User.hasMany(Post, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
 
-Project.belongsTo(User, {
+Post.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Project };
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+  hooks:true
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE',
+  hooks: true
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+  hooks:true
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE',
+  hooks:true
+})
+
+// Export the modules
+module.exports = { User, Post, Comment };
